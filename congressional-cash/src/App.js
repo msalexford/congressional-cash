8bc904fb6e331678028f115c8c5ff2917e6f0a7d
msalexford <alefor1@gmail.com>
2022-07-20 22:18:06 -0500

    Trying to fetch XML / convert to JSON in React.

--- congressional-cash/src/App.js
+++ congressional-cash/src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg'
 import './App.css'
+// import hooks useState and useState from react
+import { useState, useEffect } from 'react'
+// import axios to fetch api
+import axios from 'axios'
+// import global variables
+import { BASE_URL } from './globals'
+// import XML parser since data is in XML format
+import XMLParser from 'react-xml-parser'
+
+// set up useEffect to support async operations
 
 function App() {
+  // useEffect takes in two arguments: (1) an anonymous function, followed by (2) a dependency array
+  // whatever happens inside of this function will happen right when the component loads
+  useEffect(() => {
+    // we can put an async function inside of useEffect
+    const getData = async () => {
+      // make axios call
+      // const response = await axios.get(
+      //   `${BASE_URL}?method=independentExpend&apikey=${process.env.REACT_APP_API_KEY}`
+      // )
+      const response = await axios.get(
+        `${BASE_URL}?method=independentExpend&apikey=13a3028563e595c462745aa8615ab7d8`
+      )
+      console.log(response)
+
+      // // attempt to invoke xml parser
+      // let XMLParser = require('react-xml-parser')
+      // let xml = new XMLParser().parseFromString(response)
+    }
+    // call getData function (we want our function to fire as soon as the component loads)
+    getData()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
--- congressional-cash/src/globals.js
+++ congressional-cash/src/globals.js
@@ -1,1 +1,4 @@
-export const BASE_URL = 'https://www.opensecrets.org/api/'
+// whenever we use export, it exports these variables as an object
+// this is why when we import we destructure it out of the object
+
+export const BASE_URL = 'http://www.opensecrets.org/api/'
